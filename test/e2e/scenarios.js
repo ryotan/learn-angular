036c5f7e0c15dd83042ab4b3e2b086b2bc186c03
Ryo TANAKA <ryotan@ucb-jp.net>
2014-01-12 20:05:52 +0900

    queryをタイトルとかに表示したりしてみた。

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -26,5 +26,17 @@ describe('PhoneCat App', function() {
       input('query').enter('motorola');
       expect(repeater('.phones li').count()).toBe(2);
     });
+
+    it('should display the current filter value within an element with id "status"',
+       function() {
+         expect(element('#status').text()).toMatch(/Current filter: \s*$/);
+
+         input('query').enter('nexus');
+
+         expect(element('#status').text()).toMatch(/Current filter: nexus\s*$/);
+
+         //alternative version of the last assertion that tests just the value of the binding
+         using('#status').expect(binding('query')).toBe('nexus');
+       });
   });
 });
